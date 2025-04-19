44ce58cd87b5dfc3761d25955dabd6fe10b619b8
Cmd_ <uchzhezherov@gmail.com>
2025-04-19 21:55:08 +0300

    Removed redundant filter

--- CommonSecurity/src/main/java/org/cmd/WarehouseManager/CommonSecurity/filter/JwtFilter.java
+++ CommonSecurity/src/main/java/org/cmd/WarehouseManager/CommonSecurity/filter/JwtFilter.java
@@ -5,17 +5,19 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.cmd.WarehouseManager.CommonSecurity.service.TokenVersionService;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.JwtPrincipal;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.JwtTokenParser;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.TokenResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class JwtFilter extends GenericFilterBean {
@@ -23,12 +25,23 @@ public class JwtFilter extends GenericFilterBean {
     private TokenResolver tokenResolver;
     @Autowired
     private JwtTokenParser tokenParser;
+    @Autowired
+    private TokenVersionService tokenVersionService;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         String token = tokenResolver.resolveToken((HttpServletRequest) request);
         if (token != null && !token.isBlank()) {
             JwtPrincipal principal = tokenParser.parse(token);
+            String username = principal.getUsername();
+            Integer tokenVersion = principal.getTokenVersion();
+            Integer actualTokenVersion = tokenVersionService.getTokenVersion(username);
+            if (!Objects.equals(tokenVersion, actualTokenVersion)) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Токен устарел");
+                return;
+            }
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(
                             principal,
