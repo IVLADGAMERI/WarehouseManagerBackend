ac5a010b74a4c1709d37c8fa82ff9c05e658266d
Cmd_ <uchzhezherov@gmail.com>
2025-04-19 21:54:13 +0300

    Fixed configuration scan trouble

--- AuthenticationService/src/main/java/com/cmd/WarehouseManager/AuthenticationService/AuthenticationServiceApplication.java
+++ AuthenticationService/src/main/java/com/cmd/WarehouseManager/AuthenticationService/AuthenticationServiceApplication.java
@@ -2,8 +2,15 @@ package com.cmd.WarehouseManager.AuthenticationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"org.cmd.WarehouseManager",
+		"com.cmd.WarehouseManager",
+		"com.cmd.WarehouseManager.AuthenticationService.config;"
+	}
+)
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
