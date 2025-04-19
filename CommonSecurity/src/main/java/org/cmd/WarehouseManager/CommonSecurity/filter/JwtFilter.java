ea2967377760d6dacd840df3ef55f263b5cfeb0e
Cmd_ <uchzhezherov@gmail.com>
2025-04-19 21:00:34 +0300

    Fixed incorrect logic

--- CommonSecurity/src/main/java/org/cmd/WarehouseManager/CommonSecurity/filter/JwtFilter.java
+++ CommonSecurity/src/main/java/org/cmd/WarehouseManager/CommonSecurity/filter/JwtFilter.java
@@ -2,43 +2,41 @@ package org.cmd.WarehouseManager.CommonSecurity.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.JwtPrincipal;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.JwtTokenParser;
 import org.cmd.WarehouseManager.CommonSecurity.token.jwt.TokenResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
 @Component
-public class JwtFilter extends OncePerRequestFilter {
+public class JwtFilter extends GenericFilterBean {
     @Autowired
     private TokenResolver tokenResolver;
     @Autowired
     private JwtTokenParser tokenParser;
+
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String token = tokenResolver.resolveToken(request);
-        if (token == null || token.isBlank()) {
-            throw new BadCredentialsException("Не удалось авторизовать запрос");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String token = tokenResolver.resolveToken((HttpServletRequest) request);
+        if (token != null && !token.isBlank()) {
+            JwtPrincipal principal = tokenParser.parse(token);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            principal,
+                            null,
+                            principal.getAuthorities()
+                    );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        JwtPrincipal principal = tokenParser.parse(token);
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(
-                        principal,
-                        null,
-                        principal.getAuthorities()
-                );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
 }
